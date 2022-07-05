4eeca0837e8d4b25dca427ece111f58e4d49ec2a
C-V-Anish <mahadevrao77@gmail.com>
2022-07-05 21:25:50 +0530

    practice

--- TreeHeight.cpp
+++ TreeHeight.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 class Node
 {
@@ -14,12 +15,28 @@ int height(Node* root)
     else
     return max(height(root->left),height(root->right))+1;
 }
+int size(Node* root)
+{
+    if(root == NULL)
+    return 0;
+    else
+    {
+        return size(root->left)+size(root->right)+1;
+    }
+}
 Node* newNode(int data)
 {
     Node* temp = new Node();
     temp->key = data;
     temp->left = temp->right = NULL;
     return temp;
+}
+int getMax(Node* root)
+{
+    if(root==NULL)
+    return -1;
+    else
+    return max(root->key,max(getMax(root->left),getMax(root->right)));
 }
 int main()
 {
@@ -34,5 +51,17 @@ int main()
     root->right->right = newNode(90);
     root->right->right->left = newNode(100);
     root->right->right->right = newNode(110);
-    cout<<"Height of this tree is "<<height(root);
+    int ch;
+    cout<<"Press:-\n1 for Height\n2 for Size\n3 for Maximum\n";
+    cin>>ch;
+    switch(ch)
+    {
+        case 1 : cout<<"Height of this tree is "<<height(root);
+                 break;
+        case 2 : cout<<"Size of this tree is "<<size(root);
+                 break;
+        case 3 : cout<<"Maximum Value in tree is "<<getMax(root);
+                 break;
+        default : cout<<"Invalid Choice";
+    }
 }
--- TreeTraversals.cpp
+++ TreeTraversals.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 class Node
 {
@@ -41,17 +42,91 @@ void postorder(Node* root)
             cout<<(root->key)<<" ";
         }
 }
+void levelOrderTraversal(Node* root)
+{
+    if(root==NULL)
+    cout<<"Zero Element Tree";
+    else
+    {
+        queue<Node*> a;
+        a.push(root);
+        while(a.empty()==false)
+        {
+            Node* p = a.front();
+            a.pop();
+            cout<<(p->key)<<" ";
+            if(p->left != NULL)
+            a.push(p->left);
+            if(p->right != NULL)
+            a.push(p->right);
+        }
+    }
+}
+void LevelOrderTraversal(Node* root)
+{
+    if(root==NULL)
+    return;
+    else
+    {
+        queue<Node*> a;
+        a.push(root);
+        while(a.empty()==false)
+        {
+            int q = a.size();
+            for(int i=0;i<q;i++)
+            {
+                Node* p = a.front();
+                a.pop();
+                cout<<(p->key)<<" ";
+                if(p->left != NULL)
+                a.push(p->left);
+                if(p->right != NULL)
+                a.push(p->right);
+            }
+        }
+    }
+}
+void leftView(Node* root)
+{
+    if(root==NULL)
+    return;
+    else
+    {
+        queue<Node*> q;
+        q.push(root);
+        while(q.empty()==false)
+        {
+            int k=q.size();
+            Node* a;
+            for(int i=0;i<k;i++)
+            {
+                a=q.front();
+                q.pop();
+                if(i==0)
+                cout<<a->key<<" ";
+                if(a->left!=NULL)
+                q.push(a->left);
+                if(a->right!=NULL)
+                q.push(a->right);
+            }
+        }
+    }
+}
 int main()
 {
     Node* root = newNode(10);
     root->left = newNode(20);
     root->left->left = newNode(30);
     root->left->right = newNode(40);
-    root->right = newNode(50);
-    root->right->left = newNode(60);
-    root->right->right = newNode(70);
+    root->left->right->left = newNode(50);
+    root->left->right->left->right = newNode(60);
+    root->right = newNode(70);
+    root->right->left = newNode(80);
+    root->right->right = newNode(90);
+    root->right->right->left = newNode(100);
+    root->right->right->right = newNode(110);
     int ch;
-    cout<<"Press:-\n1 for Pre-Order Traversal\n2 for In-Order Traversal\n3 for Post-Order Traversal\n";
+    cout<<"Press:-\n1 for Pre-Order Traversal\n2 for In-Order Traversal\n3 for Post-Order Traversal\n4 for Level-Order Traversal(Method 1)\n5 for Level-Order Traversal(Method 2)\n6 for Left View of the tree\n";
     cin>>ch;
     switch(ch)
     {
@@ -60,6 +135,12 @@ int main()
         case 2 : inorder(root);
                  break;
         case 3 : postorder(root);
+                 break;
+        case 4 : levelOrderTraversal(root);
+                 break;
+        case 5 : LevelOrderTraversal(root);
+                 break;
+        case 6 : leftView(root);
                  break;
         default : cout<<"Invalid Choice";
     }
